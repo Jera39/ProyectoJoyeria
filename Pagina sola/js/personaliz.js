c2b3378c0639ba8f8e7a0432c7a4e78481f20393
Isai24 <isai.huiza.cristobal@gmail.com>
2023-11-30 18:27:08 -0500

    actualiza huiza6

--- Pagina sola/js/personaliz.js
+++ Pagina sola/js/personaliz.js
@@ -1,5 +1,3 @@
-
-
     function carga() {
         var comboBox = document.getElementById("opciones");
         var cuadroTexto = document.getElementById("cuadroTexto");
@@ -16,6 +14,26 @@
             document.getElementById('Mens').innerHTML="  Longitud de Pulsera:  ";
             cuadroTexto.style.display = "flex"; // Oculta el cuadro de texto
             mensaje.style.display = "block"; // Oculta el mensaje
+        } else {
+            cuadroTexto.style.display = "none"; // Oculta el cuadro de texto
+            mensaje.style.display = "none"; // Oculta el mensaje
+        }
+    }
+
+    function mostrarPiedras() {
+        var roca = document.getElementById("roca");
+        var piedras2 = document.getElementById("piedras2");
+        var prueba1 = document.getElementById("prueba1");
+        
+
+
+        // Verifica qué radio button está seleccionado
+        if (roca.checked) {
+            prueba1.style.display = "block"; // Muestra el cuadro de texto
+            mensaje.style.display = "none"; // Oculta el mensaje
+        } else if (piedras2.checked) {
+            prueba1.style.display = "none"; // Oculta el cuadro de texto
+            mensaje.style.display = "block"; // Muestra el mensaje
         } else {
             cuadroTexto.style.display = "none"; // Oculta el cuadro de texto
             mensaje.style.display = "none"; // Oculta el mensaje
