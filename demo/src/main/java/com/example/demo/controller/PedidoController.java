56685c78e710178d93537a36f967b20a67052593
Jera <jerameelperezm@gmail.com>
2023-12-05 20:10:01 -0500

    cambios de html

--- demo/src/main/java/com/example/demo/controller/PedidoController.java
+++ demo/src/main/java/com/example/demo/controller/PedidoController.java
@@ -10,18 +10,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.PedidoModel;
 import com.example.demo.service.PedidoService;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/pedidos")
 public class PedidoController {
 
     @Autowired
-    private PedidoService pedidoService;
+    PedidoService pedidoService;
 
     @GetMapping()
     public ArrayList<PedidoModel> obtenerPedido() {
