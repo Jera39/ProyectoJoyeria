fc311c1b043a151f2835e25d9ea49ca1b9722b19
Jera <jerameelperezm@gmail.com>
2023-12-17 18:41:37 -0500

    actualizacion para pasar a realizar el login

--- demo/src/main/resources/static/js/script.js
+++ demo/src/main/resources/static/js/script.js
@@ -1,5 +1,5 @@
 //Ejecutando funciones
-document.getElementById("btn_lista").addEventListener("click", iniciarSesion);
+document.getElementById("btn__iniciar-sesion").addEventListener("click", iniciarSesion);
 document.getElementById("btn__registrarse").addEventListener("click", register);
 window.addEventListener("resize", anchoPage);
 
@@ -12,7 +12,22 @@ var caja_trasera_register = document.querySelector(".caja__trasera-register");
 
 //FUNCIONES
 
+function anchoPage() {
 
+    if (window.innerWidth > 850) {
+        caja_trasera_register.style.display = "block";
+        caja_trasera_login.style.display = "block";
+    } else {
+        caja_trasera_register.style.display = "block";
+        caja_trasera_register.style.opacity = "1";
+        caja_trasera_login.style.display = "none";
+        formulario_login.style.display = "block";
+        contenedor_login_register.style.left = "0px";
+        formulario_register.style.display = "none";
+    }
+}
+
+anchoPage();
 
 
 function iniciarSesion() {
